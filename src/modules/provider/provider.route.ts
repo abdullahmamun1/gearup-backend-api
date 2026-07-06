a132b9e5f6ade8461f4e635fbc3b33d0bfe51c91
abdullahmamun1 <hello.abdullahmamun1@gmail.com>
2026-07-07 04:33:38 +0600

    update provider order status endpoint added

--- src/modules/provider/provider.route.ts
+++ src/modules/provider/provider.route.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { providerController } from "./provider.controller";
+import { auth } from "../../middleware/auth";
+import { Role } from "../../../generated/prisma/client";
 
 const router = Router();
 
-router.get("/orders", providerController.getProviderOrders);
+router.get(
+  "/orders",
+  auth(Role.PROVIDER),
+  providerController.getProviderOrders,
+);
+router.patch(
+  "/orders/:orderId",
+  auth(Role.PROVIDER),
+  providerController.updateOrderStatus,
+);
 
 export const providerRoutes = router;
